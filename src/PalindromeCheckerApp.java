400d13a025d997fba0ad65e3e0037bf0e9c5c0bf
ps7757 <ps7757@srmist.edu.in>
2026-02-25 09:03:16 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,17 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("--- Welcome to the Palindrome Checker App ---");
+        checkHardcoded();
+    }
+
+    public static void checkHardcoded() {
+        String word = "radar";
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println("Result: " + word + " is a palindrome.");
+        } else {
+            System.out.println("Result: " + word + " is NOT a palindrome.");
+        }
     }
 }
